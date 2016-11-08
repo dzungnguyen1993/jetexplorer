7aa4fc0d73bb604ce41df32c6981a464846a15e4
Thanh-Dung Nguyen <dz.fit.hcmus@gmail.com>
2016-11-08 11:34:29 +0700

    Create project. Add cocoapods.

---
+++ Jetex/Jetex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jetex
+//
+//  Created by Thanh-Dung Nguyen on 11/8/16.
+//  Copyright © 2016 Thanh-Dung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
