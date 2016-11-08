6009c5f29a33d57d869608cc88e7e39d03629a45
Thanh-Dung Nguyen <dz.fit.hcmus@gmail.com>
2016-11-08 17:57:14 +0700

    Add view no internet connection.

--- Jetex/Jetex/Controllers/FlightSearchVC.swift
+++ Jetex/Jetex/Controllers/FlightSearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FlightSearchVC.swift
 //  Jetex
 //
 //  Created by Thanh-Dung Nguyen on 11/8/16.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FlightSearchVC: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +23,3 @@ class ViewController: UIViewController {
 
 
 }
-
---
+++ Jetex/Jetex/Controllers/TabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarController.swift
+//  Jetex
+//
+//  Created by Thanh-Dung Nguyen on 11/8/16.
+//  Copyright © 2016 Thanh-Dung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        viewControllers = [UIViewController]()
+        self.addTabFlight()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Tab Flight
+    func addTabFlight() {
+        let vc = FlightSearchVC(nibName: "FlightSearchVC", bundle: nil)
+        let nav = UINavigationController(rootViewController: vc)
+        
+        nav.setNavigationBarHidden(true, animated: true)
+        
+        viewControllers?.append(nav)
+        
+        nav.tabBarItem = UITabBarItem(title: "Flight", image: UIImage(), tag: 1)
+    }
+}
